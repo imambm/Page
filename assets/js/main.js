1e9c1acaa5a1b2da5af1d69411cf9645ec162c18
Nauvaladc <nauvalaadc@gmail.com>
2024-10-07 13:58:35 +0700

    penambahan fungsi read more pada hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,94 +32,103 @@
     }
   };
 
-  // read more function
-  function myFunction() {
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById(myBtn);
+  // ===== responsive navbar
+  let navbarToggler = document.querySelector("#navbarToggler");
+  const navbarCollapse = document.querySelector("#navbarCollapse");
 
+  navbarToggler.addEventListener("click", () => {
+    navbarToggler.classList.toggle("navbarTogglerActive");
+    navbarCollapse.classList.toggle("hidden");
+  });
 
+  //===== close navbar-collapse when a  clicked
+  document
+    .querySelectorAll("#navbarCollapse ul li:not(.submenu-item) a")
+    .forEach((e) =>
+      e.addEventListener("click", () => {
+        navbarToggler.classList.remove("navbarTogglerActive");
+        navbarCollapse.classList.add("hidden");
+      })
+    );
 
-    // ===== responsive navbar
-    let navbarToggler = document.querySelector("#navbarToggler");
-    const navbarCollapse = document.querySelector("#navbarCollapse");
-
-    navbarToggler.addEventListener("click", () => {
-      navbarToggler.classList.toggle("navbarTogglerActive");
-      navbarCollapse.classList.toggle("hidden");
+  // ===== Sub-menu
+  const submenuItems = document.querySelectorAll(".submenu-item");
+  submenuItems.forEach((el) => {
+    el.querySelector("a").addEventListener("click", () => {
+      el.querySelector(".submenu").classList.toggle("hidden");
     });
+  });
 
-    //===== close navbar-collapse when a  clicked
-    document
-      .querySelectorAll("#navbarCollapse ul li:not(.submenu-item) a")
-      .forEach((e) =>
-        e.addEventListener("click", () => {
-          navbarToggler.classList.remove("navbarTogglerActive");
-          navbarCollapse.classList.add("hidden");
-        })
-      );
+  // ===== Features accordion
+  // const parentContainer = document.querySelector('.read-more-container');
+  // parentContainer.addEventListener('click', event => {
+  //   const current = event.target;
+  //   const isReadMoreBtn = current.className.includes('Read-more-btn');
+  //   if (!isReadMoreBtn) return;
+  //   const currentText = event.target.parentNode.querySelector('.Read-more-text');
+  //   currentText.classList.toggle('Read-more-text--show');
+  //   current.textContent = current.textContent.includes('Read More') ? "Read Less" : "Read More";
+  // })
 
-    // ===== Sub-menu
-    const submenuItems = document.querySelectorAll(".submenu-item");
-    submenuItems.forEach((el) => {
-      el.querySelector("a").addEventListener("click", () => {
-        el.querySelector(".submenu").classList.toggle("hidden");
-      });
+  // ===== Faq accordion
+  const faqs = document.querySelectorAll(".single-faq");
+  faqs.forEach((el) => {
+    el.querySelector(".faq-btn").addEventListener("click", () => {
+      el.querySelector(".icon").classList.toggle("rotate-180");
+      el.querySelector(".faq-content").classList.toggle("hidden");
     });
+  });
 
-    // ===== Features accordion
-    // const parentContainer = document.querySelector('.read-more-container');
-    // parentContainer.addEventListener('click', event => {
-    //   const current = event.target;
-    //   const isReadMoreBtn = current.className.includes('Read-more-btn');
-    //   if (!isReadMoreBtn) return;
-    //   const currentText = event.target.parentNode.querySelector('.Read-more-text');
-    //   currentText.classList.toggle('Read-more-text--show');
-    //   current.textContent = current.textContent.includes('Read More') ? "Read Less" : "Read More";
-    // })
+  // ===== wow js
+  new WOW().init();
 
-    // ===== Faq accordion
-    const faqs = document.querySelectorAll(".single-faq");
-    faqs.forEach((el) => {
-      el.querySelector(".faq-btn").addEventListener("click", () => {
-        el.querySelector(".icon").classList.toggle("rotate-180");
-        el.querySelector(".faq-content").classList.toggle("hidden");
-      });
-    });
+  // ====== scroll top js
+  function scrollTo(element, to = 0, duration = 500) {
+    const start = element.scrollTop;
+    const change = to - start;
+    const increment = 20;
+    let currentTime = 0;
 
-    // ===== wow js
-    new WOW().init();
+    const animateScroll = () => {
+      currentTime += increment;
 
-    // ====== scroll top js
-    function scrollTo(element, to = 0, duration = 500) {
-      const start = element.scrollTop;
-      const change = to - start;
-      const increment = 20;
-      let currentTime = 0;
+      const val = Math.easeInOutQuad(currentTime, start, change, duration);
 
-      const animateScroll = () => {
-        currentTime += increment;
+      element.scrollTop = val;
 
-        const val = Math.easeInOutQuad(currentTime, start, change, duration);
-
-        element.scrollTop = val;
-
-        if (currentTime < duration) {
-          setTimeout(animateScroll, increment);
-        }
-      };
-
-      animateScroll();
-    }
-
-    Math.easeInOutQuad = function (t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return (c / 2) * t * t + b;
-      t--;
-      return (-c / 2) * (t * (t - 2) - 1) + b;
+      if (currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      }
     };
 
-    document.querySelector(".back-to-top").onclick = () => {
-      scrollTo(document.documentElement);
-    };
+    animateScroll();
   }
-})
+
+  Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  };
+
+  document.querySelector(".back-to-top").onclick = () => {
+    scrollTo(document.documentElement);
+  };
+});
+
+// read more function
+const moreTextEl = document.getElementById("more-text");
+const toggleBtnEl = document.getElementById("toggle-btn");
+const hideBtnEl = document.getElementById("hide-btn");
+
+toggleBtnEl.addEventListener("click", () => {
+  moreTextEl.classList.toggle("hidden");
+  toggleBtnEl.classList.toggle("hidden");
+  hideBtnEl.classList.toggle("hidden");
+});
+
+hideBtnEl.addEventListener("click", () => {
+  moreTextEl.classList.toggle("hidden");
+  toggleBtnEl.classList.toggle("hidden");
+  hideBtnEl.classList.toggle("hidden");
+});
