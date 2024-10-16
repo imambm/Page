91f18ea23c43832ad1eac897471b714c5dd1daae
Nauvaladc <nauvalaadc@gmail.com>
2024-10-16 22:23:49 +0700

    memodifikasi hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,18 +117,18 @@
 });
 
 // read more function
-const moreTextEl = document.getElementById("more-text");
-const toggleBtnEl = document.getElementById("toggle-btn");
-const hideBtnEl = document.getElementById("hide-btn");
+// const moreTextEl = document.getElementById("more-text");
+// const toggleBtnEl = document.getElementById("toggle-btn");
+// const hideBtnEl = document.getElementById("hide-btn");
 
-toggleBtnEl.addEventListener("click", () => {
-  moreTextEl.classList.toggle("hidden");
-  toggleBtnEl.classList.toggle("hidden");
-  hideBtnEl.classList.toggle("hidden");
-});
+// toggleBtnEl.addEventListener("click", () => {
+//   moreTextEl.classList.toggle("hidden");
+//   toggleBtnEl.classList.toggle("hidden");
+//   hideBtnEl.classList.toggle("hidden");
+// });
 
-hideBtnEl.addEventListener("click", () => {
-  moreTextEl.classList.toggle("hidden");
-  toggleBtnEl.classList.toggle("hidden");
-  hideBtnEl.classList.toggle("hidden");
-});
+// hideBtnEl.addEventListener("click", () => {
+//   moreTextEl.classList.toggle("hidden");
+//   toggleBtnEl.classList.toggle("hidden");
+//   hideBtnEl.classList.toggle("hidden");
+// });
